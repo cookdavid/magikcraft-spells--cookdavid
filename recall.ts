3013868b646cc96d0a1a7e826c729942ac0c62a4
cookdavid <cook_david@yahoo.com>
2017-12-01 09:51:48 +0000

    Update recall.ts

--- recall.ts
+++ recall.ts
@@ -1,6 +1,6 @@
 const magik = magikcraft.io;
 
-function recall(){
-    const memory = magik.memento.getItem();
+function recall(name){
+    const memory = magik.memento.getItem(name);
     magik.dixit(memory);
 }
