f3f3be2e98327fbd6446a703da1a94927803da24
cookdavid <cook_david@yahoo.com>
2017-12-01 09:17:13 +0000

    Update teleport.ts

---
+++ teleport.ts
@@ -0,0 +1,10 @@
+const magik = magikcraft.io;
+
+function teleport() {
+    const there = magik.memento.getItem();
+    if (there) {
+        magik.ianuae(there);
+    } else {
+        const here = magik.hic();
+        magik.memento(here);
+    }
